8d8df634d38b908f5284cdbfcbb530ff6e1ff6a6
Francis Thérien <francis.therien@usherbrooke.ca>
2019-10-07 17:29:56 -0400

    1D PID test OK

--- tests/test_pidcontrol.py
+++ tests/test_pidcontrol.py
@@ -53,46 +53,37 @@ def test_sdof_prop():
 
 def test_sdof_pid():
     """Check PID controller outputs"""
-    tf = 2
-    npts = 500
-    tau = 1E-2
+    tf = 2 # simulation time
+    npts = 500 # simulation samples
+    tau = 1E-2 # time constant of derivative filter
 
+    # Create system/controller and run simulation
     sys = FirstOrder(1)
-    ctl = PIDController([[2]], [[1]], [[0]], dv_tau=tau)
+    ctl = PIDController([[3]], [[2]], [[0.6]], dv_tau=tau)
     clsys = ctl + sys
-
     sim = clsys.compute_trajectory(x0_sys=0, r=step(1), tf=tf, n=npts)
-    #sys.plot_trajectory(sim, 'xu')
 
     # error
     e = (sim.r - sim.y)[:, 0]
 
-    # Check that integrator values are OK
+    # Calculate integral of error
     e_int = integrate.cumtrapz(e, sim.t, initial=0)
-    assert np.allclose(sim.x[:, 1], e_int, atol=1e-8, rtol=1E-3)
 
-    # Check controller output
-    ctl_ref = (e * ctl.KP + e_int * ctl.KI).reshape((sim.n,))
-    assert np.allclose(sim.u[:, 0], ctl_ref, atol=0, rtol=1E-4)
-
-    numder = _deriv(e, sim.t)
+    # Calculate filtered derivative of error
+    numder = _deriv(e, sim.t) # centered numerical derivative
+    # Filter design
     nyqfreq = npts / tf / 2
     w0 = (1/tau/2/np.pi)  / nyqfreq
-    print(w0)
     lowpass = signal.iirfilter(1, w0, btype='lowpass', analog=False)
+    # Apply filter to derivative values
     filtered_der = signal.lfilter(*lowpass, numder)
 
-    ctlder = (e - sim.x[:, 2]) / ctl.dv_tau
+    # Ignore first 60 ms, derivative initial conditions are slightly off
+    ignore_start = 15
 
-    from matplotlib import pyplot as plt
-    plt.figure()
-    plt.plot(sim.t, e, label="error")
-    plt.plot(sim.t, ctlder, '.', label="ctl filtered deriv")
-    plt.plot(sim.t, numder, label="numerical error deriv")
-    plt.plot(sim.t, filtered_der, label="filtered deriv")
-    plt.legend()
-    plt.show()
-
+    # Check controller output
+    ctl_ref = (e * ctl.KP + e_int * ctl.KI + filtered_der * ctl.KD).reshape((sim.n,))
+    assert np.allclose(sim.u[ignore_start:, 0], ctl_ref[ignore_start:], atol=0, rtol=1E-3)
 
 if __name__ == "__main__":
-    test_sdof_pid()
+    pass
