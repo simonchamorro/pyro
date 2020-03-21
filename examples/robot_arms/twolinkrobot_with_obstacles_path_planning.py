8d5ee86ce87df665de3d4fb33f5e2aa909dc6ec7
Alexandre Girard <alx87grd@gmail.com>
2020-03-20 23:55:38 -0400

    testing manipulators with obstacles

---
+++ examples/robot_arms/twolinkrobot_with_obstacles_path_planning.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 16:34:44 2019
+
+@author: Alexandre
+"""
+
+###############################################################################
+import numpy as np
+###############################################################################
+from pyro.planning import randomtree
+from pyro.dynamic  import manipulator
+###############################################################################
+
+torque_controlled_robot = manipulator.TwoLinkManipulatorwithObstacles()
+speed_controlled_robot  = manipulator.SpeedControlledManipulator.from_manipulator( torque_controlled_robot )
+
+q_start = np.array([0,0])
+q_goal  = np.array([1.57,0])
+
+planner = randomtree.RRT( speed_controlled_robot , q_start )
+
+t = 0.2
+    
+planner.u_options = [
+        np.array([ +t, -t]),
+        np.array([ -t, +t]),
+        np.array([ 0,+t]),
+        np.array([ 0,-t]),
+        np.array([+t, 0]),
+        np.array([-t, 0])
+        ]
+
+planner.goal_radius = 0.1
+planner.max_nodes   = 20000
+
+planner.find_path_to_goal( q_goal )
+
+planner.plot_tree()
+planner.plot_open_loop_solution()
+
+speed_controlled_robot.traj = planner.trajectory
+speed_controlled_robot.animate_simulation()
