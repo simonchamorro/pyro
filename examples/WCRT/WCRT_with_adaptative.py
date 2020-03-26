95480b04b3b921e1f4dfd4341f208c6cecb2f74c
pierrecaillouette <51487008+pierrecaillouette@users.noreply.github.com>
2020-03-26 14:05:53 -0400

    Update WCRT_with_adaptative.py

--- examples/WCRT/WCRT_with_adaptative.py
+++ examples/WCRT/WCRT_with_adaptative.py
@@ -15,18 +15,18 @@ sys = WCRT.B3DDL()
 ctl  = nonlinear.AdaptativeController_WCRT(sys)
 
 #Param adapt-control
-ctl.A[0] = 0
-ctl.A[1] = 0
-ctl.A[2] = 0
-ctl.A[3] = 0
-ctl.A[4] = 0
-ctl.A[5] = 0
+ctl.A[0] = 3
+ctl.A[1] = 60
+ctl.A[2] = 21
+ctl.A[3] = 31
+ctl.A[4] = 380
+ctl.A[5] = 31
 
 ctl.Kd[0,0] = 5
 ctl.Kd[1,1] = 8
 ctl.Kd[1,1] = 5
 
-ctl.lam = 1.4
+ctl.lam = 1.8
 
 ctl.T[0,0] = 12
 ctl.T[1,1] = 12
@@ -43,9 +43,9 @@ cl_sys = ctl + sys
 #cl_sys = sys
 
 # Simultation
-cl_sys.x0  = np.array([0,1,1,1,0,0])
+cl_sys.x0  = np.array([0,1,0,0,0,0])
 tf = 12
 n = tf*1000 + 1
 cl_sys.compute_trajectory(tf, n, 'euler')
-cl_sys.plot_trajectory('xu')
+cl_sys.plot_trajectory()
 cl_sys.animate_simulation(is_3d = True)
