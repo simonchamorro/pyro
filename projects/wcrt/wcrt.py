fix sign error in wcrt

projects/wcrt/wcrt.py
@@ -90,8 +90,9 @@ class WCRT( mechanical.MechanicalSystem ):
         H = np.zeros((3,3))
         
         H[0,0] = self.m1*self.lc1**2 + self.m2*self.l1**2 + self.m3*self.l1**2
-        H[1,0] = self.m3*self.l1*self.l2*s2 + self.m3*self.l1*self.lc3*c3*s2 + self.m2*self.l1*self.lc2*s2
-        H[2,0] = self.m3*self.l1*self.lc3*s23
+        H[1,0] = - ( self.m3*self.l1*self.l2*s2 + self.m3*self.l1*self.lc3*c3*s2
+                    + self.m2*self.l1*self.lc2*s2 )
+        H[2,0] = - ( self.m3*self.l1*self.lc3*s23 )
         H[0,1] = H[1,0]
         H[1,1] = self.m2*self.lc2**2 + self.m3*self.l2**2 + 2*self.m3*self.l2*self.lc3*c3 + self.m3*self.lc3**2*c3*c3
         H[2,1] = self.m3*self.lc3*self.l2*c3 + self.m3*self.lc3**2*c3*c3
@@ -121,10 +122,13 @@ class WCRT( mechanical.MechanicalSystem ):
         C[0,0] = 0
         C[1,0] = 0
         C[2,0] = 0
-        C[0,1] = self.m2*self.l1*self.lc2*c2*dq[1] + self.m3*self.l1*self.l2*c2*dq[1] + self.m3*self.l1*self.lc3*c2*c3*dq[1] - self.m3*self.l1*self.lc3*s2*s3*dq[2]
+        C[0,1] = - ( self.m2*self.l1*self.lc2*c2*dq[1] + self.m3*self.l1*self.l2*c2*dq[1] 
+                    + self.m3*self.l1*self.lc3*c2*c3*dq[1] 
+                    - self.m3*self.l1*self.lc3*s2*s3*dq[2] )
         C[1,1] = -self.m3*self.l2*self.lc3*s3*dq[2] - self.m3*self.lc3**2*s3*c3*dq[2]
         C[2,1] = self.m3*self.l1*self.lc3*c2*c3*dq[0] + self.m3*self.l2*self.lc3*s3*dq[1] + self.m3*self.lc3**2*s3*c3*dq[1] 
-        C[0,2] = self.m3*self.l1*self.lc3*c23*dq[1] + self.m3*self.l1*self.lc3*c23*dq[2]
+        C[0,2] = - ( self.m3*self.l1*self.lc3*c23*dq[1] 
+                    + self.m3*self.l1*self.lc3*c23*dq[2] )
         C[1,2] = -C[2,1] - self.m3*self.l2*self.lc3*s3*dq[2] - 2*self.m3*self.lc3**2*s3*c3*dq[2]
         C[2,2] = 0
       
@@ -235,7 +239,7 @@ class WCRT( mechanical.MechanicalSystem ):
         pts[2,1] = self.l1*c1 - self.l2*c2*s1
         pts[2,2] = self.l2*s2
         
-        pts[3,0] = self.l1*s1 + self.l2*c1*c2 -self.l3*(c1*s2*s2-c1*c2*c3)
+        pts[3,0] = self.l1*s1 + self.l2*c1*c2 -self.l3*(c1*s2*s3-c1*c2*c3)
         pts[3,1] = self.l1*c1 - self.l2*c2*s1 + self.l3*(s1*s2*s3-s1*c2*c3)
         pts[3,2] = self.l2*s2 + self.l3*(c2*s3+c3*s2)
         
@@ -259,9 +263,10 @@ if __name__ == "__main__":
     
     sys = WCRT()
     
-    q = np.array([0.1,0.1,0.1])
-    sys.show3( q )
+    # No damping
+    sys.d1 = 0
+    sys.d2 = 0
+    sys.d3 = 0
     
-    sys.x0 = np.array([0.1,0.1,0,0,0,0])
-    sys.plot_trajectory()
+    sys.x0 = np.array([0.,1.6,0.,0,0,0])
     sys.animate_simulation( is_3d = True )