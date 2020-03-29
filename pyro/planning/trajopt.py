00102c7af2e61c42370fa57408bdf03ab0579bbb
DESKTOP-3D37VR5\Charles Khazoom <charles.khazoom@usherbrooke.ca>
2020-03-29 12:34:57 -0400

    comment all unuseful stuff to be commented later. separate function to compute dynamics constring and function to unpack decision variables. make function to compute cost (last value of cumulated cost function from Trajectory)

--- pyro/planning/trajopt.py
+++ pyro/planning/trajopt.py
@@ -23,60 +23,58 @@ from pyro.analysis import Trajectory
 #        costfunction.QuadraticCostFunction.__init__( self )
 #
 #cost_fun = TorqueSquaredCostFunction()
-
-x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
-res = minimize(rosen, x0, method='Nelder-Mead', tol=1e-6)
-res.x
-
-res2 = minimize(rosen, x0, method='SLSQP', bounds=None, constraints=())
-
-fun3 = lambda x: (x[0] - 1)**2 + (x[1] - 2.5)**2
-
-def f(x):
-    #return (x[0] - 1)**2 + (x[1] - 2.5)**2
-    return x[0] - 2 * x[1] + 2
-
-cons = ({'type': 'eq', 'fun': f},{'type': 'ineq', 'fun': lambda x: -x[0] + 2 * x[1] + 2})
-# see this for further examples on contrained optimization problems : https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html 
-
-x0_0 = [1.3, 0.7]
-
-bnds_arr = np.array([[0,None],[0,None]])
-bnds=tuple(map(tuple, bnds_arr))
-#bnds = ((0, None), (0, None))
-
-
-res3 = minimize(fun3, x0_0, method='SLSQP', bounds=bnds, constraints=cons,tol=1e-6)
-res3
+#
+#x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
+#res = minimize(rosen, x0, method='Nelder-Mead', tol=1e-6)
+#res.x
+#
+#res2 = minimize(rosen, x0, method='SLSQP', bounds=None, constraints=())
+#
+#fun3 = lambda x: (x[0] - 1)**2 + (x[1] - 2.5)**2
+#
+#def f(x):
+#    #return (x[0] - 1)**2 + (x[1] - 2.5)**2
+#    return x[0] - 2 * x[1] + 2
+#
+#cons = ({'type': 'eq', 'fun': f},{'type': 'ineq', 'fun': lambda x: -x[0] + 2 * x[1] + 2})
+## see this for further examples on contrained optimization problems : https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html 
+#
+#x0_0 = [1.3, 0.7]
+#
+#bnds_arr = np.array([[0,None],[0,None]])
+#bnds=tuple(map(tuple, bnds_arr))
+#
+#
+#res3 = minimize(fun3, x0_0, method='SLSQP', bounds=bnds, constraints=cons,tol=1e-6)
+#res3
 '''
 ################################################################################
 '''
 # create system 
 sys = manipulator.TwoLinkManipulator()
 
-# PATCH : must compute trajectory before passing it to OpenLoopController() class constructor
-# create open-loop controller
-tf=4
-sys.compute_trajectory( tf )
-
-#ctl  = plan.OpenLoopController.load_from_file( 'double_pendulum_rrt.npy' )
-ctl = plan.OpenLoopController(sys.traj)
-ctl.trajectory.u=np.ones(sys.traj.u.shape)*10 # create inputs
-
-
-sys.f(np.array([1,2,3,4]),np.array([3,2])) # compute f
-sys.x0 =np.array([np.pi/2,np.pi/2,0,0])
-
-
-# New cl-dynamic
-cl_sys = ctl + sys
-# cl_sys.m is 1 should be 2 ??
-
-cl_sys.compute_trajectory( tf )
-cl_sys.plot_trajectory('xu')
-
-sys.cost_function.trajectory_evaluation(sys.traj)
-#cl_sys.animate_simulation()
+## PATCH : must compute trajectory before passing it to OpenLoopController() class constructor
+## create open-loop controller
+#tf=4
+#sys.compute_trajectory( tf )
+#
+##ctl  = plan.OpenLoopController.load_from_file( 'double_pendulum_rrt.npy' )
+#ctl = plan.OpenLoopController(sys.traj)
+#ctl.trajectory.u=np.ones(sys.traj.u.shape)*10 # create inputs
+#
+#
+#sys.f(np.array([1,2,3,4]),np.array([3,2])) # compute f
+#sys.x0 =np.array([np.pi/2,np.pi/2,0,0])
+#
+#
+## New cl-dynamic
+#cl_sys = ctl + sys
+## cl_sys.m is 1 should be 2 ??
+#
+#cl_sys.compute_trajectory( tf )
+#cl_sys.plot_trajectory('xu')
+#sys.cost_function.trajectory_evaluation(sys.traj)
+##cl_sys.animate_simulation()
 
 # 
 '''
@@ -124,7 +122,7 @@ create initial guess
 
 '''
 
-x_guess = np.linspace(ub_x0, ub_xF, ngrid)
+x_guess = np.linspace(ub_x0, ub_xf, ngrid)
 u_guess = np.ones([ngrid,sys.m])
 t_guess = np.linspace(ub_t0, ub_tf, ngrid)
 y_guess= x_guess
@@ -192,32 +190,63 @@ def compute_dx(x,u,sys):
     global ngrid 
     for i in range(ngrid):
         dx[i,:]=sys.f(x[i,:],u[i,:])
-    
     return dx
     
 
-def dynamics_cstr(decision_variables):
+def dec_var_2_traj(decision_variables):
     global ngrid        
-    x,u,t0,tf = unpack_dec_var(decision_variables)
-    h = (tf-t0)/(ngrid-1) #step between each grid
-    dx = compute_dx(x,u,sys)
-    dyn_constr = np.diff(x,axis=0) - (dx[0:-1]+dx[1:])*h/2 # this must be = 0
-    return dyn_constr
     
+    x,u,t0,tf = unpack_dec_var(decision_variables)
+    
+    dx = compute_dx(x,u,sys)
+    
+    t = np.linspace(t0, tf, ngrid)
+    
+    y=x # find proper fct evaluation later from sys object
+    
+    traj = Trajectory(x, u, t, dx, y)
+    
+    return traj
+
+def dynamics_cstr(traj):
+    
+    t0 = traj.t[0]
+    
+    tf = traj.t[-1]
+    
+    h = (tf-t0)/(ngrid-1) #step between each grid
+    
+    dyn_constr = np.diff(traj.x,axis=0) - (traj.dx[0:-1]+traj.dx[1:])*h/2 # this must be = 0
+    
+    return dyn_constr
+
+
+dec_vars = np.linspace(0,301,302)
+
+
+#traj_opt =  dec_var_2_traj(dec_vars)   
+yo = dynamics_cstr(dec_var_2_traj(dec_vars))
+
 #dynamics_cstr(A)
 #    sys.f(np.array([1,2,3,4]),np.array([3,2])) # compute f
 
-cons = ({'type': 'eq', 'fun': dynamics_cstr(x)})
+cons = ({'type': 'eq', 'fun': lambda x: dynamics_cstr(dec_var_2_traj(x))})
+
 
 #dec_var = # decision variables
 #optim_traj = Trajectory(x_opt, u_opt, t_opt, dx_opt, y_opt)
 #lb_dec_var = ub_x*ones(x.shape)
 
+def compute_cost(decision_variables):
+    traj_opt = dec_var_2_traj(dec_vars) #get trajectorty
+    traj_opt = sys.cost_function.trajectory_evaluation(traj_opt)#compute cost fcn
+    cost_function = traj_opt.J[-1]# traj_opt.J is the cumulated cost from integral, we take only the last value
+    return cost_function
 '''
 Solve non-linear program
 '''
 
-res4 = minimize(fun3, x0_0, method='SLSQP', bounds=bnds, constraints=cons,tol=1e-6)
+res4 = minimize(compute_cost, x0_0, method='SLSQP', bounds=bnds, constraints=cons,tol=1e-6)
 
 
 
