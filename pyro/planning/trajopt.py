499ab4acac4ec38316bfce5151b915d4dc7e59e6
DESKTOP-3D37VR5\Charles Khazoom <charles.khazoom@usherbrooke.ca>
2020-03-29 16:17:09 -0400

    try to use 'trust-constr' optim method instead of 'slsqp' but does not work yet.

--- pyro/planning/trajopt.py
+++ pyro/planning/trajopt.py
@@ -8,7 +8,7 @@ Created on Wed Jul 12 12:09:37 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.optimize import minimize, rosen, rosen_der#, interpolate
+from scipy.optimize import minimize, NonlinearConstraint, rosen, rosen_der#, interpolate
 from pyro.dynamic import manipulator
 from pyro.planning import plan
 from pyro.analysis import costfunction
@@ -21,62 +21,14 @@ from pyro.analysis import Trajectory
 #class TorqueSquaredCostFunction( costfunction.QuadraticCostFunction ):
 #    def __init__(self):
 #        costfunction.QuadraticCostFunction.__init__( self )
-#
-#cost_fun = TorqueSquaredCostFunction()
-#
-#x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
-#res = minimize(rosen, x0, method='Nelder-Mead', tol=1e-6)
-#res.x
-#
-#res2 = minimize(rosen, x0, method='SLSQP', bounds=None, constraints=())
-#
-#fun3 = lambda x: (x[0] - 1)**2 + (x[1] - 2.5)**2
-#
-#def f(x):
-#    #return (x[0] - 1)**2 + (x[1] - 2.5)**2
-#    return x[0] - 2 * x[1] + 2
-#
-#cons = ({'type': 'eq', 'fun': f},{'type': 'ineq', 'fun': lambda x: -x[0] + 2 * x[1] + 2})
-## see this for further examples on contrained optimization problems : https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html 
-#
-#x0_0 = [1.3, 0.7]
-#
-#bnds_arr = np.array([[0,None],[0,None]])
-#bnds=tuple(map(tuple, bnds_arr))
-#
-#
-#res3 = minimize(fun3, x0_0, method='SLSQP', bounds=bnds, constraints=cons,tol=1e-6)
-#res3
+
 '''
 ################################################################################
 '''
 # create system 
 sys = manipulator.TwoLinkManipulator()
 
-## PATCH : must compute trajectory before passing it to OpenLoopController() class constructor
-## create open-loop controller
-#tf=4
-#sys.compute_trajectory( tf )
-#
-##ctl  = plan.OpenLoopController.load_from_file( 'double_pendulum_rrt.npy' )
-#ctl = plan.OpenLoopController(sys.traj)
-#ctl.trajectory.u=np.ones(sys.traj.u.shape)*10 # create inputs
-#
-#
-#sys.f(np.array([1,2,3,4]),np.array([3,2])) # compute f
-#sys.x0 =np.array([np.pi/2,np.pi/2,0,0])
-#
-#
-## New cl-dynamic
-#cl_sys = ctl + sys
-## cl_sys.m is 1 should be 2 ??
-#
-#cl_sys.compute_trajectory( tf )
-#cl_sys.plot_trajectory('xu')
-#sys.cost_function.trajectory_evaluation(sys.traj)
-##cl_sys.animate_simulation()
 
-# 
 '''
 Create optization problem
 '''
@@ -171,6 +123,7 @@ def pack_bounds(lb_x,ub_x,lb_x0,ub_x0,lb_xf,ub_xf,lb_u,ub_u,lb_t0,ub_t0,lb_tf,ub
     
     return bnds
 
+
 def traj_2_dec_var(traj):
     
     dec_vars = np.array([]).reshape(0,1) # initialize dec_vars array
@@ -239,41 +192,45 @@ def dynamics_cstr(traj):
     h = (tf-t0)/(ngrid-1) #step between each grid
     
     dyn_constr = np.diff(traj.x,axis=0) - (traj.dx[0:-1]+traj.dx[1:])*h/2 # this must be = 0
-    
+    dyn_constr = dyn_constr.reshape((ngrid-1)*sys.n,)
     return dyn_constr
 
 def compute_cost(decision_variables):
-    traj_opt = dec_var_2_traj(dec_vars) #get trajectorty
+    traj_opt = dec_var_2_traj(decision_variables) #get trajectorty
     traj_opt = sys.cost_function.trajectory_evaluation(traj_opt)#compute cost fcn
     cost_function = traj_opt.J[-1]# traj_opt.J is the cumulated cost from integral, we take only the last value
     return cost_function
 
 
-dec_vars = np.linspace(0,301,302)
-
-
-#traj_opt =  dec_var_2_traj(dec_vars)   
-yo = dynamics_cstr(dec_var_2_traj(dec_vars))
-
-#dynamics_cstr(A)
-#    sys.f(np.array([1,2,3,4]),np.array([3,2])) # compute f
-
 bnds = pack_bounds(lb_x,ub_x0,lb_x,ub_x0,lb_xf,ub_xf,lb_u,ub_u,lb_t0,lb_t0,lb_tf,lb_tf)
-cons = ({'type': 'eq', 'fun': lambda x: dynamics_cstr(dec_var_2_traj(x))})
+cons_slsqp = ({'type': 'eq', 'fun': lambda x: dynamics_cstr(dec_var_2_traj( x ))  })
+
+cons=NonlinearConstraint(lambda x: dynamics_cstr(dec_var_2_traj( x )), 0, 0)#
 
 dec_var_guess = traj_2_dec_var(guess_traj)
 
-#dec_var = # decision variables
-#optim_traj = Trajectory(x_opt, u_opt, t_opt, dx_opt, y_opt)
-#lb_dec_var = ub_x*ones(x.shape)
 
 
 '''
 Solve non-linear program
 '''
 
-res4 = minimize(compute_cost, dec_var_guess, method='SLSQP', bounds=bnds, constraints=cons,tol=1e-6)
+res4 = minimize(compute_cost, dec_var_guess, method='trust-constr', bounds=bnds, constraints=cons,tol=1e-6,jac='2-point',hess='cs',options={'disp': True})
 
+
+result_traj = dec_var_2_traj(res4.x)
+
+ctl = plan.OpenLoopController(result_traj)
+#
+sys.traj=result_traj
+
+## New cl-dynamic
+cl_sys = ctl + sys
+#
+#cl_sys.compute_trajectory( tf )
+sys.plot_trajectory('xu')
+#sys.cost_function.trajectory_evaluation(sys.traj)
+##cl_sys.animate_simulation()
 
 
 '''
