dbf968d0857dadac978513a2ca8ff7925448e5f1
Alexandre Girard <alx87grd@gmail.com>
2020-05-17 16:38:00 -0400

    bug fix

--- examples/robot_arms/twolinkrobot_kinematic_controller.py
+++ examples/robot_arms/twolinkrobot_kinematic_controller.py
@@ -13,8 +13,7 @@ from pyro.dynamic  import manipulator
 ###############################################################################
 
 torque_controlled_robot = manipulator.TwoLinkManipulator()
-speed_controlled_robot  = manipulator.SpeedControlledManipulator( 
-                                             torque_controlled_robot )
+speed_controlled_robot  = manipulator.SpeedControlledManipulator.from_manipulator( torque_controlled_robot )
 
 
 kinematic_controller = robotcontrollers.EndEffectorKinematicController( speed_controlled_robot , 1 )
